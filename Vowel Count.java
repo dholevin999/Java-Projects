7a64f6db73216e0af24b540ac2a4c7db31845a4e
dholevin999 <dholevin999@gmail.com>
2016-09-24 20:59:47 -0400

    Add files via upload

---
+++ Vowel Count.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+  public static void main(String []args){
+  Scanner reader = new Scanner(System.in); // Reading from System.in
+  System.out.println("Enter a word or sentance: ");
+  String word = reader.nextLine().toLowerCase();
+  int aCount = 0;
+  int eCount = 0;
+  int iCount = 0;
+  int oCount = 0;
+  int uCount = 0;
+  int yCount = 0;
+
+  for (int i = 0; i < word.length(); i++) {
+  if (word.charAt(i) == 'a') {
+  aCount++;
+  } else if (word.charAt(i) == 'e') {
+  eCount++;
+  } else if (word.charAt(i) == 'i') {
+  iCount++;
+  } else if (word.charAt(i) == 'o') {
+  oCount++;
+  } else if (word.charAt(i) == 'u') {
+  uCount++;
+  } else if (word.charAt(i) == 'y') {
+  yCount++;
+  }
+  }
+  int totalCount = aCount + eCount + iCount + oCount + uCount + yCount;
+  System.out.println("There are " + totalCount + " total vowels in the word.");
+  System.out.println(aCount + " a's, " + eCount + " e's, " + iCount + " i's, " + oCount + " o's, " + uCount + " u's, and " + yCount + " y's");
+
+  }
+
+}
