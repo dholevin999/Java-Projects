07f8dc9c7d955a994eaf8695a8d3060be78328c8
dholevin999 <dholevin999@gmail.com>
2016-10-13 13:20:06 -0400

    formatting changes

--- Palindrome.java
+++ Palindrome.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Palindrome {
   
   public static void main(String []args){
-  Scanner reader = new Scanner(System.in); // Reading from System.in
-  System.out.println("Enter a word: ");
-  String word = reader.next();
-  String reverseWord = "";
+    Scanner reader = new Scanner(System.in); // Reading from System.in
+    System.out.println("Enter a word: ");
+    String word = reader.next();
+    String reverseWord = "";
     
-  for (int i = 0; i < word.length(); i++) {
-  reverseWord += word.charAt(word.length()-1-i);
-  }
-  if (word.equals(reverseWord)) {
-  System.out.println("its a palindrome");
-  } else {
-  System.out.println("not a palindrome");
-  }
+    for (int i = 0; i < word.length(); i++) {
+      reverseWord += word.charAt(word.length()-1-i);
+    }
+      if (word.equals(reverseWord)) {
+      System.out.println("its a palindrome");
+    } else {
+      System.out.println("not a palindrome");
+    }
     
-}
+  }
 }
