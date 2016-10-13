592a7d1eee037f991be57a0d36aac868080113d3
dholevin999 <dholevin999@gmail.com>
2016-10-13 13:19:32 -0400

    formatting changes

--- Password Generator.java
+++ Password Generator.java
@@ -6,6 +6,7 @@ public class PasswordGenerator{
   public static void main(String []args){
   Scanner reader = new Scanner(System.in);
   System.out.println("Enter the length of your password:");
+    
   int count = reader.nextInt();
   int randomNum = 0;
   int numOrLetter = 0;
@@ -15,31 +16,31 @@ public class PasswordGenerator{
   String symbols = "@#$%:?";
 
   for (int i = 0; i< count; i++) {
-  numOrLetter = ThreadLocalRandom.current().nextInt(0, 3);
+    numOrLetter = ThreadLocalRandom.current().nextInt(0, 3);
 
-  if (numOrLetter == 0) {
-  randomNum = ThreadLocalRandom.current().nextInt(0, 9);
-  numString = Integer.toString(randomNum);
-  password += numString;
-  } else if (numOrLetter == 1){
-  upperOrLower = ThreadLocalRandom.current().nextInt(0, 2);
-  if (upperOrLower == 0) {
-  Random r = new Random();
-  char c = (char) (r.nextInt(26) + 'A');
-  password += c;
-  } else {
-  Random r = new Random();
-  char c = (char) (r.nextInt(26) + 'a');
-  password += c;
-  }
-  } else {
-  int randomSymbol = ThreadLocalRandom.current().nextInt(0, symbols.length());
-  char sym = symbols.charAt(randomSymbol);
-  password += sym;
-  }
-  }
+    if (numOrLetter == 0) {
+      randomNum = ThreadLocalRandom.current().nextInt(0, 9);
+      numString = Integer.toString(randomNum);
+      password += numString;
+    } else if (numOrLetter == 1){
+      upperOrLower = ThreadLocalRandom.current().nextInt(0, 2);
+      if (upperOrLower == 0) {
+        Random r = new Random();
+        char c = (char) (r.nextInt(26) + 'A');
+        password += c;
+      } else {
+        Random r = new Random();
+        char c = (char) (r.nextInt(26) + 'a');
+        password += c;
+      }
+      } else {
+      int randomSymbol = ThreadLocalRandom.current().nextInt(0, symbols.length());
+      char sym = symbols.charAt(randomSymbol);
+      password += sym;
+      }
+    }
 
-  System.out.println(password);
+    System.out.println(password);
   }
 
 }
